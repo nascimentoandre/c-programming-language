9cc9deeaea8fb0265cf14be0150555f1471bc70d
André Nascimento <andrevsn1@gmail.com>
2021-03-14 15:17:57 -0300

    finish

--- chapter_1/17.c
+++ chapter_1/17.c
@@ -9,20 +9,25 @@ than 80 characters.
 #define MAXCHAR 10000 // maximum number of input characters
 
 void selectivePrint(int s[]) {
-  int lineCount, lower, i;
+  int lineCount, lower, i, j;
   lineCount = lower = i = 0;
 
+  printf("Printing lines longer than 80 characters:\n\n");
+
   while (s[i] != '\0') {
-    if (s[i] != '\n')
+    if (s[i] != '\n') {
       lineCount++;
-    else {
-      if (lineCount < 80) {
-        for (int j = lower; j <= i; j++)
-          printf("%c", s[j]);
+      i++;
+    } else {
+      if (lineCount > 80) {
+        for (j = lower; j < i; j++) {
+          putchar(s[j]);
+        }
+        putchar('\n');
       }
       lineCount = 0;
-      lower = i+1; // check this, probably will cause a bug
       i++;
+      lower = i;
     }
   }
 }
@@ -38,7 +43,6 @@ int main() {
 
   input[i+1] = '\0';
   selectivePrint(input);
+  //print(input);
   return 0;
 }
-
-
